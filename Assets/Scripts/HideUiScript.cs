2a3bdcd68bb4eec32a8e27b25603c65921283c39
PeikVisuri <pvisuriabo.fi>
2018-11-01 00:14:21 +0200

    Moved objects and added hide ui button
    Hide ui button also hides itself so it cant be reversed :P

---
+++ Assets/Scripts/HideUiScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideUiScript : MonoBehaviour {
+
+    public void hideUI()
+    {
+        gameObject.SetActive(false);
+            
+    }
+}
