6f3f5e6c2579f4ccf2e185d7850a8c6c89678431
niemisami <sami@nieminen.fi>
2018-10-31 08:56:05 +0200

    Display visual aid for next notes

--- Assets/Scripts/ArrowEventController.cs
+++ Assets/Scripts/ArrowEventController.cs
@@ -1,44 +1,117 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Text;
 using UnityEngine.UI;
 
 public class ArrowEventController
 {
-  Dictionary<string, ArrowEventAction> beatEventMap;
+  Dictionary<KeyCode, ArrowEventAction> keyActionMap;
+  Dictionary<string, KeyCode> characterKeyMap;
   private string[] oldStrokes;
-  private string[] strokes;
+  private string[] songCorrectCharacters;
   private int nextIndex = 0;
+  private Text nextNotesText;
 
   private Up upAction;
   private Down downAction;
   private Left leftAction;
   private Right rightAction;
+  private Fail failAction;
+  private End endAction;
 
-  public void init(string[] strokes)
+  private StringBuilder sb;
+
+  public void init(string[] songCorrectCharacters, Text nextNotesText)
   {
-    beatEventMap = new Dictionary<string, ArrowEventAction>();
 
+
+    // Map characters to correct Input KeyCodes
+    characterKeyMap = new Dictionary<string, KeyCode>();
+    characterKeyMap.Add("W", KeyCode.UpArrow);
+    characterKeyMap.Add("A", KeyCode.LeftArrow);
+    characterKeyMap.Add("S", KeyCode.DownArrow);
+    characterKeyMap.Add("D", KeyCode.RightArrow);
+
+    // Arrays of song chars; "beats" and directions
+    oldStrokes = new string[songCorrectCharacters.Length];
+    this.songCorrectCharacters = songCorrectCharacters;
+
+    // Initialize commands objects which updates the UI
     upAction = new Up();
-    downAction = new Down();
     leftAction = new Left();
+    downAction = new Down();
     rightAction = new Right();
+    failAction = new Fail();
+    endAction = new End();
 
-    beatEventMap.Add("W", upAction);
-    beatEventMap.Add("A", leftAction);
-    beatEventMap.Add("S", downAction);
-    beatEventMap.Add("D", rightAction);
-    oldStrokes = new string[strokes.Length];
-    this.strokes = strokes;
+    // Map Input KeyCode to a certain command
+    keyActionMap = new Dictionary<KeyCode, ArrowEventAction>();
+    keyActionMap.Add(KeyCode.UpArrow, upAction);
+    keyActionMap.Add(KeyCode.LeftArrow, leftAction);
+    keyActionMap.Add(KeyCode.DownArrow, downAction);
+    keyActionMap.Add(KeyCode.RightArrow, rightAction);
+
+    // Text element for displaying next notes
+    this.nextNotesText = nextNotesText;
+    displayNextNotes(6);
   }
 
-  public ArrowEventAction getNextAction()
+  /**
+  * Evaluate whether passed KeyCode matches the correct one next on the line
+  **/
+  public ArrowEventAction getNextAction(KeyCode pressedKey)
   {
-    string nextKey = strokes[nextIndex];
-    oldStrokes[nextIndex] = nextKey;
+    // Show Game Over if no notes available
+    if (nextIndex == songCorrectCharacters.Length)
+    {
+      return endAction;
+    }
+    string nextCharacter = songCorrectCharacters[nextIndex];
     nextIndex++;
-    return beatEventMap[nextKey];
+    displayNextNotes(3);
+
+    KeyCode expectedKeyCode = characterKeyMap[nextCharacter];
+    if (pressedKey == expectedKeyCode)
+    {
+      oldStrokes[nextIndex] = nextCharacter;
+      return keyActionMap[expectedKeyCode];
+    }
+    return failAction;
   }
+
+  /*
+  * Visual aid for displaying what should be pressed next
+  **/
+  void displayNextNotes(int display)
+  {
+    string nextCharacter = songCorrectCharacters[nextIndex];
+    string nextText = "";
+    int hintIndex = nextIndex - 1;
+    try
+    {
+      for (int i = 0; i < display; i++)
+      {
+        nextText += characterKeyMap.ContainsKey(songCorrectCharacters[++hintIndex])
+          ? characterKeyMap[songCorrectCharacters[hintIndex]].ToString()
+          : " Pause" + " ";
+      }
+    }
+    catch (KeyNotFoundException e)
+    {
+      Debug.LogError(e.ToString());
+      nextText += " Pause";
+    }
+    catch (IndexOutOfRangeException e)  // CS0168
+    {
+      nextText += " END";
+    }
+
+    nextText += "...";
+    nextNotesText.text = nextText;
+  }
+
 }
 
 public abstract class ArrowEventAction
@@ -50,27 +123,41 @@ public class Up : ArrowEventAction
 {
   public override void Execute(Text textContainer)
   {
-    textContainer.text = "Next: UP";
+    textContainer.text = "UP";
   }
 }
 public class Left : ArrowEventAction
 {
   public override void Execute(Text textContainer)
   {
-    textContainer.text = "Next: LEFT";
+    textContainer.text = "LEFT";
   }
 }
 public class Right : ArrowEventAction
 {
   public override void Execute(Text textContainer)
   {
-    textContainer.text = "Next: RIGHT";
+    textContainer.text = "RIGHT";
   }
 }
 public class Down : ArrowEventAction
 {
   public override void Execute(Text textContainer)
   {
-    textContainer.text = "Next: Down";
+    textContainer.text = "Down";
+  }
+}
+public class Fail : ArrowEventAction
+{
+  public override void Execute(Text textContainer)
+  {
+    textContainer.text = "FAIL!";
+  }
+}
+public class End : ArrowEventAction
+{
+  public override void Execute(Text textContainer)
+  {
+    textContainer.text = "GAME OVER!";
   }
 }
--- Assets/Scripts/SongBookController.cs
+++ Assets/Scripts/SongBookController.cs
@@ -10,54 +10,53 @@ public class SongBookController : MonoBehaviour
   public int alpha = 5;
 
   public Text keyStrokeText;
+  public Text nextNotesText;
 
   private ArrowEventController arrowController;
   private float startTime;
   private float elapsedTime;
-  public string[] beats;
+  public string[] songMappedCharacters;
+  public KeyCode[] allowedKeys;
+  private KeyCode lastKey;
 
   void Awake()
   {
+    arrowController = new ArrowEventController();
   }
   void Start()
   {
-    beats = new string[] { "A", "W", "S", "0", "D", "W", "S", "W", "A", "W", "S", "0", "D", "W", "S", "0", "A", "S", "W", "S", "D", "S", "W", "S", "A", "W", "S", "0", "D", "W", "S", "W" };
+    songMappedCharacters = new string[] { "A", "W", "S", "0", "D", "W", "S", "W", "A", "W", "S", "0", "D", "W", "S", "0", "A", "S", "W", "S", "D", "S", "W", "S", "A", "W", "S", "0", "D", "W", "S", "W" };
+    allowedKeys = new KeyCode[] { KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.RightArrow };
 
-    arrowController = new ArrowEventController();
-
-    arrowController.init(beats);
+    arrowController.init(songMappedCharacters, nextNotesText);
     startTime = Time.time;
     displayText("Nothing");
   }
 
   void Update()
   {
-    // if (Input.GetKeyUp(KeyCode.UpArrow))
-    // {
-    //   displayText("UP");
-    // }
-    // if (Input.GetKeyUp(KeyCode.DownArrow))
-    // {
-    //   displayText("DOWN");
-    // }
-    // if (Input.GetKeyUp(KeyCode.LeftArrow))
-    // {
-    //   displayText("LEFT");
-    // }
-    // if (Input.GetKeyUp(KeyCode.RightArrow))
-    // {
-    //   displayText("RIGHT");
-    // }
-    elapsedTime = Time.time - startTime;
-    Debug.Log(elapsedTime);
-    Console.WriteLine(elapsedTime > 1);
-    if (elapsedTime > 1.0)
+    foreach (KeyCode code in allowedKeys)
     {
-      Debug.Log("HERE");
-      ArrowEventAction nextBeat = arrowController.getNextAction();
-      nextBeat.Execute(keyStrokeText);
-      startTime = Time.time;
+      if (Input.GetKeyUp(code))
+      {
+        // TODO: use actual beats from sheet
+        // elapsedTime = Time.time - startTime;
+        // if (elapsedTime > 1.0)
+        // {
+        try
+        {
+          ArrowEventAction nextBeat = arrowController.getNextAction(code);
+          nextBeat.Execute(keyStrokeText);
+        }
+        catch (KeyNotFoundException)
+        {
+          Debug.LogWarning("Pause");
+        }
+        startTime = Time.time;
+      }
+      // }
     }
+
   }
 
   void displayText(string stroke)
