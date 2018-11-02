7b34803d204493299fbaca24be1b485a2ac0bfc9
PeikVisuri <pvisuriabo.fi>
2018-11-02 20:16:53 +0200

    Moved menu scripts and sprites to own folders, and made small changes to the menu screen

---
+++ Assets/Scripts/Menu Scripts/SpinningScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningScript : MonoBehaviour {
+    public float spinningAmount = 0f;
+	// Update is called once per frame
+	void FixedUpdate () {
+        spinningAmount += 0.5f;
+        transform.rotation = Quaternion.Euler(0,  0, spinningAmount);
+    }
+}
