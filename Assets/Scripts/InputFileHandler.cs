1c61a6b89bb509c5f9e474062ce2cf00e9020aa7
Leenus <LeenusGit@users.noreply.github.com>
2018-12-07 00:04:51 +0200

    Removed duration and bpm lines because they are not needed and the input file is much easier to modify without them.

--- Assets/Scripts/InputFileHandler.cs
+++ Assets/Scripts/InputFileHandler.cs
@@ -20,24 +20,26 @@ public class InputFileHandler {
 		
 		for (var i = 0; i < data.Length; i++) {
 
-			if (i == 0) {
-				var durationArray = data[i].Split(":".ToCharArray());
-				var minutes = int.Parse(durationArray[0].Trim());
-				var seconds = float.Parse(durationArray[1].Trim());
+//			if (i == 0) {
+//				var durationArray = data[i].Split(":".ToCharArray());
+//				var minutes = int.Parse(durationArray[0].Trim());
+//				var seconds = float.Parse(durationArray[1].Trim());
+//
+//				DurationSeconds = minutes * 60 + seconds;
+//			} 
+//
+//			else if (i == 1) {
+//				Bpm = float.Parse(data[i].Trim());
+//			}
+//			else {
+//				
+//			}
 
-				DurationSeconds = minutes * 60 + seconds;
-			} 
-
-			else if (i == 1) {
-				Bpm = float.Parse(data[i].Trim());
-			}
-			else {
-				
-				var lineData = data[i].Split("\t".ToCharArray());
-				var timeStamp = float.Parse(lineData[0]);
-				var input = lineData[2];
-				MappingsDict.Add(timeStamp, input[0]);
-			}
+			var lineData = data[i].Split("\t".ToCharArray());
+			var timeStamp = float.Parse(lineData[0]);
+			var input = lineData[2];
+			MappingsDict.Add(timeStamp, input[0]);
+			
 		}
 	}
 }
