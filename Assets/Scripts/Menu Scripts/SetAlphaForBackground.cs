a7fdf94b2ea464bbe75186987bfde90b700f6b6c
PeikVisuri <pvisuriabo.fi>
2018-12-10 20:26:59 +0200

    Fixed end game background and added the new room sprites

--- Assets/Scripts/Menu Scripts/SetAlphaForBackground.cs
+++ Assets/Scripts/Menu Scripts/SetAlphaForBackground.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetAlphaForBackground : MonoBehaviour {
 
-    public float fadeSpeed = 0.001f;
+    public float fadeSpeed = 0.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,8 @@ public class SetAlphaForBackground : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, fadeSpeed);
+        
+
+        GetComponent<Image>().color += new Color(0, 0, 0, fadeSpeed);
     }
 }
